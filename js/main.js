da57db6b938bf253c397457911d8e166d1c54641
Johan Garcia Trejo <johan.garcia.dev@gmail.com>
2024-05-20 00:10:31 -0600

    feat: :sparkles: Implementación del juego "Piedra, Papel y Tijeras"
    - Añadida la lógica para determinar el ganador según las reglas tradicionales. - Estilizada la interfaz de usuario con CSS. - Actualizada la puntuación para llevar un registro de las victorias.

---
+++ js/main.js
@@ -0,0 +1,87 @@
+const rock = document.querySelector("#rock");
+const paper = document.querySelector("#paper");
+const scissors = document.querySelector("#scissors");
+const scoreUser = document.querySelector(".score__user");
+const scorePc = document.querySelector(".score__pc");
+let scoreUser1 = 0;
+let scorePc1 = 0;
+
+const optionsGame = ["rock", "paper", "scissors"];
+
+const rockObj = {
+  rock: "Empataste",
+  paper: "Perdiste",
+  scissors: "Ganaste",
+};
+
+const paperObj = {
+  paper: "Empataste",
+  rock: "Ganaste",
+  scissors: "Perdiste",
+};
+
+const scissorsObj = {
+  scissors: "Empataste",
+  paper: "Ganaste",
+  rock: "Perdiste",
+};
+
+const random = () => {
+  return Math.floor(Math.random() * 3);
+};
+
+const validateWinner = (userSelection) => {
+  let optionPc = optionsGame[random()];
+  let result = userSelection[optionPc];
+
+  let gameResult = {
+    optionPc,
+    result,
+  };
+
+  return gameResult;
+};
+
+const updateScore = (result) => {
+  if (result !== "Empataste") result === "Ganaste" ? scoreUser1++ : scorePc1++;
+
+  scoreUser.innerHTML = `${scoreUser1}`;
+  scorePc.innerHTML = `${scorePc1}`;
+};
+
+const showAlert = (option, result) => {
+  const icon = {
+    Ganaste: "success",
+    Perdiste: "error",
+    Empataste: "info",
+  };
+
+  Swal.fire({
+    icon: icon[result],
+    title: `${result} la pc selecciono 👆`,
+    imageUrl: `./img/${option}.png`,
+    imageAlt: `${option}`,
+    confirmButtonText: "Aceptar",
+  });
+};
+
+rock.addEventListener("click", () => {
+  let gameResult = validateWinner(rockObj);
+
+  updateScore(gameResult.result);
+  showAlert(gameResult.optionPc, gameResult.result);
+});
+
+paper.addEventListener("click", () => {
+  let gameResult = validateWinner(paperObj);
+
+  updateScore(gameResult.result);
+  showAlert(gameResult.optionPc, gameResult.result);
+});
+
+scissors.addEventListener("click", () => {
+  let gameResult = validateWinner(scissorsObj);
+
+  updateScore(gameResult.result);
+  showAlert(gameResult.optionPc, gameResult.result);
+});
